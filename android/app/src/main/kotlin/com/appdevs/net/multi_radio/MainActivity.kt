0e74b817d8fcd3a3f0caeb13fd370f6e87de9f4b
Eduardo Valencia <kalienrique40@gmail.com>
2025-02-13 16:51:03 -0600

    fix android

--- android/app/src/main/kotlin/com/appdevs/net/multi_radio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.appdevs.net.multi_radio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/compaz/spark/compaz_radio/MainActivity.kt
@@ -0,0 +1,5 @@
+package compaz.spark.compaz_radio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
